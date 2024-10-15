a5a8bd26b6844e6e3b8686d4dbc0bf734c8499ba
AJDavidR <ajdevidr@gmail.com>
2024-10-14 19:22:25 -0500

    Add:styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import './style.css'
 import { GifExpertApp } from './GifExpertApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
