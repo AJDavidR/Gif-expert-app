a921988893dc55da4acf2e84a3bfdc7862e8db32
AJDavidR <ajdevidr@gmail.com>
2024-11-28 17:29:28 -0500

    fix: gif-data

--- src/helpers/GetGifs.js
+++ src/helpers/GetGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
-  // const url = `https://api.giphy.com/v1/gifs/search?api_key=2Z6epzRK8r9bvgfjQ9KHz0TU7ozy7oTX&limit=2&q=${category}`;
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=U8uJ50LC003lXV7jpwg5i3RHYLYu1XaY&limit=2&q=${category}`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=2Z6epzRK8r9bvgfjQ9KHz0TU7ozy7oTX&limit=2&q=${category}`;
+  // const url = `https://api.giphy.com/v1/gifs/search?api_key=U8uJ50LC003lXV7jpwg5i3RHYLYu1XaY&limit=2&q=${category}`;
   const resp = await fetch(url);
   const { data = [] } = await resp.json();
   const gifs = data.map((img) => ({
@@ -8,6 +8,6 @@ export const getGifs = async (category) => {
     title: img.title,
     url: img.images.downsized_medium.url,
   }));
-console.log(gifs)
+  console.log(gifs);
   return gifs;
 };
