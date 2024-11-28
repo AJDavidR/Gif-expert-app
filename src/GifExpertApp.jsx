c77c0ce296fd22f9355e49f816c97455a9c7a3eb
AJDavidR <ajdevidr@gmail.com>
2024-11-28 16:02:14 -0500

    Add: GifGrid

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One punch", "Dragon Ball Z"]);
@@ -11,21 +12,13 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      {/* Titulo */}
       <h1>GifExpertApp</h1>
 
-      {/* Input */}
       <AddCategory onNewCategory={onAddCategory} />
 
-      {/* Listado */}
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-        {/* <li>ABC</li> */}
-      </ol>
-
-      {/* Gif Item */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
