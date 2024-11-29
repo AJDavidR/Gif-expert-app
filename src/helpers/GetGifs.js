aa3692107579259b231ece0ac1609ed69243575a
AJDavidR <ajdevidr@gmail.com>
2024-11-28 19:24:32 -0500

    fix: helper

--- src/helpers/GetGifs.js
+++ src/helpers/GetGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
-	const url = `https://api.giphy.com/v1/gifs/search?api_key=2Z6epzRK8r9bvgfjQ9KHz0TU7ozy7oTX&limit=2&q=${category}`;
-	// const url = `https://api.giphy.com/v1/gifs/search?api_key=U8uJ50LC003lXV7jpwg5i3RHYLYu1XaY&limit=2&q=${category}`;
+	// const url = `https://api.giphy.com/v1/gifs/search?api_key=2Z6epzRK8r9bvgfjQ9KHz0TU7ozy7oTX&limit=2&q=${category}`;
+	const url = `https://api.giphy.com/v1/gifs/search?api_key=U8uJ50LC003lXV7jpwg5i3RHYLYu1XaY&limit=5&q=${category}`;
 	const resp = await fetch(url);
 	const { data = [] } = await resp.json();
 	const gifs = data.map((img) => ({
