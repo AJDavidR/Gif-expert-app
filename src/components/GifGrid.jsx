a2c629d315449ec351b22ce214045ec1f121cc26
AJDavidR <ajdevidr@gmail.com>
2024-11-29 19:09:33 -0500

    Add: GifGrid test

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,18 +1,23 @@
 import { GifGridItem } from './GifGridItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
+import PropTypes from 'prop-types';
 
 export const GifGrid = ({ category }) => {
-	const { images, isLoading } = useFetchGifs(category);
+  const { images, isLoading } = useFetchGifs(category);
 
-	return (
-		<>
-			<h3>{category}</h3>
-			{isLoading && <div>Cargando...</div>}
-			<div className="card-grid">
-				{images?.map((image) => (
-					<GifGridItem key={image.id} {...image} />
-				))}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <h3>{category}</h3>
+      {isLoading && <div>Cargando...</div>}
+      <div className="card-grid">
+        {images?.map((image) => (
+          <GifGridItem key={image.id} {...image} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
 };
